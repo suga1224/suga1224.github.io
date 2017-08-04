09dc39fa94cdff359661fe88188b4e79f4ebbe76
Unknown <satouhiroki1224@gmail.com>
2017-08-04 20:56:08 +0900

    再投稿泣

--- port/main.js
+++ port/main.js
@@ -1,39 +1,14 @@
-
-//$('div.test').eq(0).html('hoge');
-/*
-var div = $('<div></div>').html('hoge');
-$('body').append(div);
-*/
-
-$('div').css('width','200px')
-        .css('height','200px')
-        .css('backgroundColor','#000000');
-$('body').append(div);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*　１０個のボックスを作る
-for(var i = 0; i<10; i++)
-{
-  var div = document.createElement('div');
-  var w = Math.random() * 300;
-  var h = 50;
-  div.style.backgroundColor = '#000000';
-  div.style.width = w + 'px';
-  div.style.height =h+'px';
-  div.style.marginBottom = '5px';
-  div.style.marginLeft = '100px';
-  document.getElementsByTagName('body')[0].appendChild(div);
-}
-*/
+$(function () {
+  var $body = $('body');
+  $('#js__sideMenuBtn').on('click', function () {
+    $body.toggleClass('side-open');
+    $('#js__overlay').on('click', function () {
+      $body.removeClass('side-open');　　
+    });
+  });
+});
+$(function() {
+    $('.grid').masonry({
+        columnWidth: 160
+    });
+});
